516e2cc1b8d9946ee145083edbc1276ac92c48f4
aokomoriuta_lab <internal@enmps.net>
2012-02-15 06:49:36 +0900

    ArticleIDLinkをResourceLoader対応

--- ArticleIDLink.js
+++ ArticleIDLink.js
@@ -1,24 +1,47 @@
-addOnloadHook(function(){mw.ready(function()
+/************************************************************************/
+// ja> = 記事IDリンク =
+//   > ツールボックス内に記事IDでの短縮URLリンクを生成する
+//   > ;作者: 青子守歌
+//   > ;URL: http://ja.wikipedia.org/wiki/User:aokomoriuta
+//   >
+//   > == ライセンス ==
+//   > このスクリプトは、複数ライセンスで利用可能です。
+//   > 選択可能なライセンスは次の通り：
+//   > * クリエイティブ・コモンズ 表示-継承 3.0 非移植
+//   > * クリエイティブ・コモンズ 表示-非営利 3.0 非移植
+//   > * GNU一般公衆利用許諾書 バージョン3.0以降
+/************************************************************************/
+// en> = Article ID Link =
+//   > This script makes a shortened link using article ID in TOOLBOX
+//   > ;author: 青子守歌
+//   > ;URL: http://ja.wikipedia.org/wiki/User:aokomoriuta
+//   >
+//   > == License ==
+//   > This script is multi-licensed.
+//   > You can select the license of your choice from as following:
+//   > * Creative Commons Attribution-ShareAlike 3.0 Unported
+//   > * Creative Commons Attribution-Noncommercial 3.0 Unported
+//   > * GNU General Public License v3.0 or later
+/************************************************************************/
+jQuery(function($)
 {
 	// メッセージ追加
-	mw.addMessages({
-		"articleidlink-link": "記事IDでのリンク"
-	});
+	mw.messages.set("articleidlink-link", "記事IDでのリンク");
 	
-	// ツールボックス内リストを取得
-	var $tblist = $j("#p-tb>.body>ul");
-	
-	// リストが取得できていれば
-	if($tblist)
+	// 版への固定リンクを取得し、取得できていれば
+	var $permalink = $("#t-permalink");
+	if($permalink)
 	{
-		// 記事IDでのリンクを追加
-		$tblist.append($j(document.createElement("li")).attr({id: "t-artcileidlink"})
-			.append($j(document.createElement("a"))
+		// 固定リンクの次に記事IDでのリンクを追加
+		$permalink.after($("<li />")
+			.attr("id", "t-artcileidlink")
+			.append($("<a />")
 				.attr({
-					title: mw.getMsg("articleidlink-link"),
-					href: "/wiki/?curid=" + wgArticleId
+					title: mw.msg("articleidlink-link"),
+					href: "/wiki/?curid=" + mw.config.get("wgArticleId")
 				})
-				.text(mw.getMsg("articleidlink-link")))
+				.text("記事IDでのリンク")
+			)
 		);
 	}
-});});
+});
